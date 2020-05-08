7d59794a2a10646f76a12da43637fb20935d2f07
monoid <qaswedfr55@gmail.com>
2020-05-08 15:24:00 +0900

    R1

---
+++ Alarm/TreeViewModel.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Publisher;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace Alarm
+{
+    interface IAlertPage
+    {
+        string ValidPageName
+        {
+            get;
+        }
+    }
+    public class DocumentDbContext : DbContext{
+        public DocumentDbContext() : base() { }
+        public DbSet<Document> Documents { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlite("Data Source=doc.db");
+        }
+    }
+    [Table("Document")]
+    public class DocumentView : Document, IAlertPage
+    {
+        public DocumentView():base()
+        {
+            /*if (!System.Reflection.Assembly.GetExecutingAssembly().Location.Contains("VisualStudio")){               
+                throw new Exception("Called Not in Design Mode");
+            }*/
+        }
+        public DocumentView(Document document):base()
+        {
+            BaseUrl = document.BaseUrl;
+            Date = document.Date;
+            GUID = document.GUID;
+            Summary = document.Summary;
+            Title = document.Title;
+        }
+        public string ValidPageName => "DocumentView.xaml";
+    }
+    public class SiteModel : INotifyPropertyChanged, IAlertPage
+    {
+        private string title;
+        private ObservableCollection<DocumentView> documents;
+        public SiteModel()
+        {
+            documents = new ObservableCollection<DocumentView>();
+        }
+        public SiteModel(string title):this()
+        {
+            this.title = title;
+        }
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public ObservableCollection<DocumentView> Documents
+        {
+            get => documents;
+            set
+            {
+                documents = value;
+            }
+        }
+
+        public string ValidPageName => "ContentListView";
+
+        public void RemoveFirstDocument()
+        {
+            documents.Remove(documents.First());
+            OnPropertyChanged("Documents");
+        }
+        public void RemoveDocument(DocumentView document)
+        {
+            OnPropertyChanged("Documents");
+        }
+        public void Add(DocumentView document)
+        {
+            documents.Add(document);
+            OnPropertyChanged("Documents");
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            Trace.WriteLine($"{propertyName} is changed!");
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    public class CategoryItem : INotifyPropertyChanged, IAlertPage
+    {
+        private string title;
+        private ObservableCollection<SiteModel> siteModels;
+        public CategoryItem()
+        {
+            this.title = "No Named Category";
+        }
+        public CategoryItem(string title)
+        {
+            this.title = title;
+        }
+        public string Title
+        {
+            get => title;
+            set
+            {
+                title = value;
+                OnPropertyChanged("Title");
+            }
+        }
+        public ObservableCollection<SiteModel> SiteModels
+        {
+            get => siteModels;
+            set
+            {
+                siteModels = value;
+                OnPropertyChanged("SiteModels");
+            }
+        }
+
+        public string ValidPageName => "CategoryView";
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+    public class TreeViewModel : ObservableCollection<CategoryItem>
+    {
+    }
+    class PageFactory
+    {
+        public static Page Generate(string name)
+        {
+            switch (name)
+            {
+                case "CategoryView":
+                    return new CategoryView();
+                case "ContentListView":
+                    return new ContentListView();
+                case "DocumentView":
+                default:
+                    return new EmptyPage();
+            }
+        }
+    }
+}
---
+++ Publisher/RSSFetcher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+
+namespace Publisher
+{
+    public class RSSFetcher : Fetcher
+    {
+        private string url;
+        public string URL => url;
+        public RSSFetcher(string url) : base(){
+            this.url = url;
+        }
+        public override Task<List<Document>> Fetch()
+        {
+            var cur_doc = GetRSS();
+            return Task.FromResult(cur_doc);
+        }
+        public List<Document> GetRSS()
+        {
+            try
+            {
+                var root = XElement.Load(url);
+                var items = root.Elements("channel").Descendants("item");
+                var doclist = from item in items
+                              let title = item.Element("title").Value
+                              let description = item.Element("description").Value
+                              let guid = item.Element("guid").Value
+                              let pubData = item.Element("pubDate").Value
+                              select DocumentBuilder.Doc()
+                                .Title(title)
+                                .Summary(description)
+                                .GUID(guid)
+                                .pubDate(DateTime.Parse(pubData))
+                                .Build();
+                return doclist.ToList();
+            }
+            catch(XmlException e)
+            {
+                Console.WriteLine(e.Message);
+                return new List<Document>();
+            }
+        }
+    }
+}
