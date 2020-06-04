79caf3e7a6ac8ed7cf1d786c0d6c44602697c335
monoid <qaswedfr55@gmail.com>
2020-06-04 19:27:47 +0900

    remove some Trace.WriteLine

--- Alarm/View/MainWindow.xaml.cs
+++ Alarm/View/MainWindow.xaml.cs
@@ -258,7 +258,6 @@ namespace Alarm.View
                     }
                 }
                 if (category == null) throw new ApplicationException("Search fail.");
-                Trace.WriteLine("Change Owner Start");
                 if (fetcherViewModel.Parent != category)
                 {
                     fetcherViewModel.ChangeOwner(category);
