a891f0ee85f0d83bb2474ffb91ec1d40a2a4e16f
monoid <qaswedfr55@gmail.com>
2020-06-10 19:54:01 +0900

    AtomFetcher get Exception

--- Publisher/AtomFetcher.cs
+++ Publisher/AtomFetcher.cs
@@ -3,6 +3,8 @@ using System.ServiceModel.Syndication;
 using System.Threading.Tasks;
 using System.Xml;
 using System.Linq;
+using System.IO;
+using System;
 
 namespace Model {
 	public class AtomFetcher : Fetcher {
@@ -18,26 +20,49 @@ namespace Model {
 		public string Uri { get => uri; set => uri = value; }
 
 		public override Task<PublishedEventArg> Fetch() {
-			var reader = XmlReader.Create(uri);
-			var feed = SyndicationFeed.Load(reader); // use SyndicationFeed to load atom feed
-			reader.Close();
+			try
+			{
+				var reader = XmlReader.Create(uri);
+				var feed = SyndicationFeed.Load(reader); // use SyndicationFeed to load atom feed
+				reader.Close();
 
-			// Build document list
-			
-			var docs = from item in feed.Items
-				let title = item.Title.Text
-				let summary = item.Summary.Text
-				let id = item.Id
-				let date = item.LastUpdatedTime.ToString()
-				select DocumentBuilder.Doc()
-					.Title(title)
-					.Summary(summary)
-					.GUID(id)
-					.pubDate(date)
-					.Build();
-			
-			return Task.FromResult(new PublishedEventArg(docs));
+				// Build document list
+				var docs = from item in feed.Items
+						   let title = item.Title.Text
+						   let summary = item.Summary?.Text ?? ""
+						   let id = item.Id
+						   let date = item.LastUpdatedTime.ToString()
+						   let url = item.Links.First()?.Uri?.ToString() ?? "about:blank"
+						   select DocumentBuilder.Doc()
+							   .Title(title)
+							   .Summary(summary)
+							   .GUID(id)
+							   .pubDate(date)
+							   .URL(url)
+							   .Build();
+
+				return Task.FromResult(new PublishedEventArg(docs));
+			}
+			catch(ArgumentException e)
+            {
+				return Task.FromResult(new PublishedEventArg(PublishedStatusCode.InvaildArgumentError, e.Message));
+			}
+			catch(UriFormatException e)
+            {
+				return Task.FromResult(new PublishedEventArg(PublishedStatusCode.InvaildFormatError, e.Message));
+            }
+			catch(FileNotFoundException e)
+            {
+				return Task.FromResult(new PublishedEventArg(PublishedStatusCode.ConnectionFailError, e.Message));
+            }
+            catch (System.Net.WebException e)
+            {
+				return Task.FromResult(new PublishedEventArg(PublishedStatusCode.ConnectionFailError, e.Message));
+            }
+			catch(Exception e)
+            {
+				return Task.FromResult(new PublishedEventArg(PublishedStatusCode.UnknownError, e.Message));
+			}
 		}
-
 	}
 }
